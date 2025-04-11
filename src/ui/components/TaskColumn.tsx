102fa3c457c5fde3900a6a9c7122d93e141e8731
Parikshit Prasai <parikshit.prasai@terahs.ai>
2025-04-11 14:19:50 +0545

    (fix) track tasks on task column component

--- src/ui/components/TaskColumn.tsx
+++ src/ui/components/TaskColumn.tsx
@@ -2,6 +2,7 @@ import { Card, CardHeader } from '@mui/material';
 import { Task, TaskStatus } from '../../domain/Task';
 import { useTaskStore } from '../../service/task/TaskContext';
 import TaskCard from './TaskCard';
+import { useEffect, useState } from 'react';
 
 interface Props {
     status: TaskStatus;
@@ -10,19 +11,29 @@ interface Props {
 
 const TaskColumn = ({ status, onCardClick }: Props) => {
     const { getTasksByStatus, updateTask } = useTaskStore();
+    const [tasks, setTasks] = useState<Task[]>(getTasksByStatus(status));
+
+    useEffect(() => {
+        setTasks(getTasksByStatus(status));
+    }, [status]);
+
+    const handleFavoriteToggle = (taskId: string) => {
+        updateTask(taskId, {
+            favorite: !tasks.find((task) => task.id === taskId)?.favorite,
+        });
+        setTasks(getTasksByStatus(status));
+    };
 
     return (
         <div className="min-w-[300px] w-full max-w-sm flex flex-col gap-2">
             <Card className="shadow-md">
                 <CardHeader title={status.replace('_', ' ')} />
             </Card>
-            {getTasksByStatus(status).map((task) => (
+            {tasks.map((task) => (
                 <TaskCard
                     key={task.id}
                     task={task}
-                    onFavoriteToggle={() =>
-                        updateTask(task.id, { favorite: !task.favorite })
-                    }
+                    onFavoriteToggle={() => handleFavoriteToggle(task.id)}
                     onClick={() => onCardClick(task)}
                 />
             ))}
